19559ea8f974845fb3c2aad45f085f1dde013441
Joni Collinge <jonathan-collinge@hotmail.com>
2016-03-03 15:13:43 +0000

    Initial commit

---
+++ ml_retraining/Program.cs
@@ -0,0 +1,334 @@
+﻿// This code requires the Nuget package Microsoft.AspNet.WebApi.Client to be installed.
+// Instructions for doing this in Visual Studio:
+// Tools -> Nuget Package Manager -> Package Manager Console
+// Install-Package Microsoft.AspNet.WebApi.Client
+//
+// Also, add a reference to Microsoft.WindowsAzure.Storage.dll for reading from and writing to the Azure blob storage
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace CallBatchExecutionService
+{
+    public class AzureBlobDataReference
+    {
+        public string ConnectionString { get; set; }
+        public string RelativeLocation { get; set; }
+        public string BaseLocation { get; set; }
+        public string SasBlobToken { get; set; }
+    }
+
+    public enum BatchScoreStatusCode
+    {
+        NotStarted,
+        Running,
+        Failed,
+        Cancelled,
+        Finished
+    }
+
+    public class BatchScoreStatus
+    {
+        // Status code for the batch scoring job
+        public BatchScoreStatusCode StatusCode { get; set; }
+
+
+        // Locations for the potential multiple batch scoring outputs
+        public IDictionary<string, AzureBlobDataReference> Results { get; set; }
+
+        // Error details, if any
+        public string Details { get; set; }
+    }
+
+    public class BatchExecutionRequest
+    {
+
+        public IDictionary<string, AzureBlobDataReference> Inputs { get; set; }
+        public IDictionary<string, string> GlobalParameters { get; set; }
+
+        // Locations for the potential multiple batch scoring outputs
+        public IDictionary<string, AzureBlobDataReference> Outputs { get; set; }
+    }
+
+    class Program
+    {
+        private static Dictionary<string, string> _configuration;
+
+        static void Main(string[] args)
+        {
+            loadConfig();
+
+            InvokeBatchExecutionService().Wait();
+        }
+
+        static private void loadConfig()
+        {
+            using (StreamReader file = File.OpenText("C:/tmp/configuration.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                _configuration = (Dictionary<string, string>)serializer.Deserialize(file, typeof(Dictionary<string, string>));
+            }
+        }
+
+        static async Task WriteFailedResponse(HttpResponseMessage response)
+        {
+            Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+            // Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+            Console.WriteLine(response.Headers.ToString());
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseContent);
+        }
+
+        static void UploadFileToBlob(string inputFileLocation, string inputBlobName, string storageContainerName, string storageConnectionString)
+        {
+            // Make sure the file exists
+            if (!File.Exists(inputFileLocation))
+            {
+                throw new FileNotFoundException(
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "File {0} doesn't exist on local computer.",
+                        inputFileLocation));
+            }
+
+            Console.WriteLine("Uploading the input to blob storage...");
+
+            var blobClient = CloudStorageAccount.Parse(storageConnectionString).CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(storageContainerName);
+            container.CreateIfNotExists();
+            var blob = container.GetBlockBlobReference(inputBlobName);
+            blob.UploadFromFile(inputFileLocation, FileMode.Open);
+        }
+
+        static void ProcessResults(BatchScoreStatus status)
+        {
+
+            foreach (var output in status.Results)
+            {
+                var blobLocation = output.Value;
+                Console.WriteLine(string.Format("The result '{0}' is available at the following Azure Storage location:\n", output.Key));
+                Console.WriteLine(string.Format("Download the new model's evaluation:\n {0}{1}{2}", blobLocation.BaseLocation, blobLocation.RelativeLocation, blobLocation.SasBlobToken));
+                Console.WriteLine();
+            }
+        }
+
+        static async Task InvokeBatchExecutionService()
+        {
+            // How this works:
+            //
+            // 1. Assume the input is present in a local file (if the web service accepts input)
+            // 2. Upload the file to an Azure blob - you'd need an Azure storage account
+            // 3. Call the Batch Execution Service to process the data in the blob. Any output is written to Azure blobs.
+            // 4. Download the output blob, if any, to local file
+
+            const string BaseUrl = "https://europewest.services.azureml.net/workspaces/caa80bb3831f44da92e0cb29eca5f63a/services/a7d4ab93946d43eb96dda1534d29ab94/jobs";
+
+            string StorageAccountName = _configuration["storageacc_name"];
+            string StorageAccountKey = _configuration["storageacc_key"];
+            string StorageContainerName = _configuration["storageacc_containername"];
+            string apiKey = _configuration["training_endpoint_key"];
+
+            // set a time out for polling status
+            const int TimeOutInMilliseconds = 120 * 1000; // Set a timeout of 2 minutes
+
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
+
+            UploadFileToBlob(_configuration["localfilepath"],
+               "input1datablob.csv",
+               StorageContainerName, storageConnectionString);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var request = new BatchExecutionRequest()
+                {
+                    Inputs = new Dictionary<string, AzureBlobDataReference>()
+                    {
+                        {
+                            "input1",
+                            new AzureBlobDataReference()
+                            {
+                                ConnectionString = storageConnectionString,
+                                RelativeLocation = string.Format("{0}/input1datablob.csv", StorageContainerName)
+                            }
+                        },
+                    },
+
+                    Outputs = new Dictionary<string, AzureBlobDataReference>()
+                    {
+                        {
+                            "output1",
+                            new AzureBlobDataReference()
+                            {
+                                ConnectionString = storageConnectionString,
+                                RelativeLocation = string.Format("/{0}/output1results.csv", StorageContainerName)
+                            }
+                        },
+                        {
+                            "output2",
+                            new AzureBlobDataReference()
+                            {
+                                ConnectionString = storageConnectionString,
+                                RelativeLocation = string.Format("/{0}/output2results.ilearner", StorageContainerName)
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                // WARNING: The 'await' statement below can result in a deadlock if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false) so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+
+
+                Console.WriteLine("Submitting the job...");
+
+                // submit the job
+                var response = await client.PostAsJsonAsync(BaseUrl + "?api-version=2.0", request);
+                if (!response.IsSuccessStatusCode)
+                {
+                    await WriteFailedResponse(response);
+                    return;
+                }
+
+                string jobId = await response.Content.ReadAsAsync<string>();
+                Console.WriteLine(string.Format("Job ID: {0}", jobId));
+
+
+                // start the job
+                Console.WriteLine("Starting the job...");
+                response = await client.PostAsync(BaseUrl + "/" + jobId + "/start?api-version=2.0", null);
+                if (!response.IsSuccessStatusCode)
+                {
+                    await WriteFailedResponse(response);
+                    return;
+                }
+
+                string jobLocation = BaseUrl + "/" + jobId + "?api-version=2.0";
+                Stopwatch watch = Stopwatch.StartNew();
+                bool done = false;
+                while (!done)
+                {
+                    Console.WriteLine("Checking the job status...");
+                    response = await client.GetAsync(jobLocation);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        await WriteFailedResponse(response);
+                        return;
+                    }
+
+                    BatchScoreStatus status = await response.Content.ReadAsAsync<BatchScoreStatus>();
+                    if (watch.ElapsedMilliseconds > TimeOutInMilliseconds)
+                    {
+                        done = true;
+                        Console.WriteLine(string.Format("Timed out. Deleting job {0} ...", jobId));
+                        await client.DeleteAsync(jobLocation);
+                    }
+                    switch (status.StatusCode)
+                    {
+                        case BatchScoreStatusCode.NotStarted:
+                            Console.WriteLine(string.Format("Job {0} not yet started...", jobId));
+                            break;
+                        case BatchScoreStatusCode.Running:
+                            Console.WriteLine(string.Format("Job {0} running...", jobId));
+                            break;
+                        case BatchScoreStatusCode.Failed:
+                            Console.WriteLine(string.Format("Job {0} failed!", jobId));
+                            Console.WriteLine(string.Format("Error details: {0}", status.Details));
+                            done = true;
+                            break;
+                        case BatchScoreStatusCode.Cancelled:
+                            Console.WriteLine(string.Format("Job {0} cancelled!", jobId));
+                            done = true;
+                            break;
+                        case BatchScoreStatusCode.Finished:
+                            done = true;
+                            Console.WriteLine(string.Format("Job {0} finished!", jobId));
+
+                            ProcessResults(status);
+                            Console.WriteLine("Overwrite existing model? <y/n>");
+                            var res = Console.ReadLine().ToLower();
+                            if (res == "y")
+                                await OverwriteModel(status);
+                            break;
+                    }
+
+                    if (!done)
+                    {
+                        Thread.Sleep(1000); // Wait one second
+                    }
+                }
+            }
+        }
+
+        private static async Task OverwriteModel(BatchScoreStatus status)
+        {
+            // status contains 2 outputs - we want the output with key Output2 which is at index 0
+            var newModelBlobLocation = status.Results.ElementAt(0).Value;
+            string apiKey = _configuration["trainable_endpoint_key"];
+            string endpointURL = _configuration["trainable_endpoint_uri"];
+
+            var resourceLocations = new
+            {
+                Resources = new[]
+                {
+                    new
+                    {
+                        Name = "Census Model [trained model]",
+                        Location = new AzureBlobDataReference()
+                        {
+                            BaseLocation = newModelBlobLocation.BaseLocation,
+                            RelativeLocation = newModelBlobLocation.RelativeLocation, //from the output, for example: “experimentoutput/8946abfd-79d6-4438-89a9-3e5d109183/8946abfd-79d6-4438-89a9-3e5d109183.ilearner”
+                            SasBlobToken = newModelBlobLocation.SasBlobToken //from the output, for example: “?sv=2013-08-15&sr=c&sig=37lTTfngRwxCcf94%3D&st=2015-01-30T22%3A53%3A06Z&se=2015-01-31T22%3A58%3A06Z&sp=rl”
+                        }
+                    }
+                }
+            };
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                using (var request = new HttpRequestMessage(new HttpMethod("PATCH"), endpointURL))
+                {
+                    request.Content = new StringContent(JsonConvert.SerializeObject(resourceLocations), System.Text.Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.SendAsync(request);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        await WriteFailedResponse(response);
+                    }
+
+                    // Do what you want with a successful response here.
+                    Console.WriteLine("Successfully updated the retraining endpoint model - Please wait 15 minutes for the service to fully migrate");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
+
